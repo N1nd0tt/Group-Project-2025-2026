79b2071e66f2b84e8203a94848c45106d9b6266c
N1nd0tt <oleksiy.lopatynskiy@gmail.com>
2025-11-11 21:23:57 +0100

    initial commit

---
+++ src/main/java/com/example/projekt_zesp/ProjektZespApplication.java
@@ -0,0 +1,13 @@
+package com.example.projekt_zesp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektZespApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektZespApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projekt_zesp/ProjektZespApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projekt_zesp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektZespApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
