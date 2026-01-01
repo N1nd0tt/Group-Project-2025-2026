afb23437992494aebe27127bd6038564df60814a
Bogdan Derdziak <bogdan.derdz@gmail.com>
2026-01-01 15:49:12 +0100

    FastAPI template

--- ai/main.py
+++ ai/main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from ai!")
+import uvicorn
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"Hello": "World"}
+
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
